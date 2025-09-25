71aecb7a4beda61e556f1da33a0d228fd741b27b
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-25 15:02:55 +0600

    update

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,8 +1,33 @@
-const mongoose= require('mongoose')
+const mongoose = require('mongoose')
 
-const productSchema= new mongoose.Schema({
-    name:{
+const productSchema = new mongoose.Schema({
+    name: {
         type: String,
-        
+        required: true,
+    },
+
+    category: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    old_price: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    new_price: {
+        type: String,
+        required: true,
     }
 })
+
+const Product=mongoose.model('products', productSchema)
+
+module.exports= Product
